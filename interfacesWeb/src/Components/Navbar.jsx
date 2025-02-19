4f37ae39389489ccab7479d6b6c84358472f9e15
JorgeIsmaelBetancourtEspericueta <164288597+JorgeIsmaelBetancourtEspericueta@users.noreply.github.com>
2025-02-18 17:13:21 -0700

    Update Navbar.jsx
    Corregí, había subido mal

--- interfacesWeb/src/Components/Navbar.jsx
+++ interfacesWeb/src/Components/Navbar.jsx
@@ -13,65 +13,70 @@ import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
-import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 import { Link } from "react-router-dom";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
+
   const menuOptions = [
     {
       text: "Home",
       icon: <HomeIcon />,
+      link: "/",
     },
     {
       text: "Misión",
       icon: <InfoIcon />,
-      link: "#about",
+      link: "/#about",
     },
     {
       text: "Testimonials",
       icon: <CommentRoundedIcon />,
-      link: "#about",
+      link: "/resena",
     },
     {
       text: "Contactános",
       icon: <PhoneRoundedIcon />,
-    },
-    {
-      text: "Cart",
-      icon: <ShoppingCartRoundedIcon />,
+      link: "/#contacto",
     },
   ];
+
   return (
     <nav>
       <div className="nav-logo-container">
-        <img src={Logo} alt="" />
+        <img src={Logo} alt="Logo" />
       </div>
       <div className="navbar-links-container">
-        <a href="">Home</a>
-        <a href="#about">Misión</a>
-        <a href="#resena">Reseñas</a>
-        <a href="#contacto">Contactános</a>
+        <Link to="/">Home</Link>
+        <a href="/#about">Misión</a>
+        <Link to="/resena">Reseñas</Link>
+        <a href="/#contacto">Contactános</a>
         <Link to="/login">
           <button className="primary-button">Login</button>
         </Link>
       </div>
       <div className="navbar-menu-container">
-        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+        <HiOutlineBars3
+          size={30}
+          onClick={() => setOpenMenu(true)}
+          style={{ cursor: "pointer" }}
+        />
       </div>
-      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={() => setOpenMenu(false)}
-          onKeyDown={() => setOpenMenu(false)}
-        >
+      <Drawer
+        open={openMenu}
+        onClose={() => setOpenMenu(false)}
+        anchor="right"
+      >
+        <Box sx={{ width: 250 }} role="presentation">
           <List>
             {menuOptions.map((item) => (
               <ListItem key={item.text} disablePadding>
                 <ListItemButton
-                  component="a"
-                  href={item.link || "#"}
+                  // Si el link contiene "#" usamos <a> con href, sino usamos Link con to
+                  component={item.link.includes("#") ? "a" : Link}
+                  href={item.link.includes("#") ? item.link : undefined}
+                  to={item.link.includes("#") ? undefined : item.link}
                   onClick={() => setOpenMenu(false)}
                 >
                   <ListItemIcon>{item.icon}</ListItemIcon>
@@ -79,8 +84,20 @@ const Navbar = () => {
                 </ListItemButton>
               </ListItem>
             ))}
+            {/* Botón de Login */}
+            <ListItem disablePadding>
+              <ListItemButton
+                component={Link}
+                to="/login"
+                onClick={() => setOpenMenu(false)}
+              >
+                <ListItemIcon>
+                  <AccountCircleIcon />
+                </ListItemIcon>
+                <ListItemText primary="Login" />
+              </ListItemButton>
+            </ListItem>
           </List>
-
           <Divider />
         </Box>
       </Drawer>
